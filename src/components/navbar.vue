16658afa2b67d06035de8bc1e2992cb80112039a
Lana <>
2019-01-07 11:13:50 +0100

    lesson 7 and 8

---
+++ src/components/navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+        <!--lesson 7 toolbars-->
+        <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Lana</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <!--lesson 8 navigation drawers-->
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                drawer: false
+            }
+        }
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+
+
+  </div>
+</template>
+
+<script>
+
+    export default {
+        components: {
+
+        }
+    }
+</script>
